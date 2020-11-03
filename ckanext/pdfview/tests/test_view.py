7450d50bdbd0ed916f02c0beab6d7740dc82961b
Jonathan Berry <jonathan.s.berry@googlemail.com>
2020-11-03 11:58:22 +0000

    ADX-458 Fix tests for ckan 2.9

--- ckanext/pdfview/tests/test_view.py
+++ ckanext/pdfview/tests/test_view.py
@@ -1,13 +1,11 @@
-import paste.fixture
 import pylons.config as config
 import urlparse
-
+import pytest
 import ckan.model as model
 import ckan.plugins as plugins
 import ckan.lib.helpers as h
 import ckanext.pdfview.plugin as plugin
 import ckan.lib.create_test_data as create_test_data
-import ckan.config.middleware as middleware
 
 try:
     import ckan.tests.legacy as tests
@@ -15,7 +13,8 @@ except ImportError:
     # CKAN <= 2.3
     import ckan.tests as tests
 
-def _create_test_view(view_type):
+
+def _create_test_view():
     context = {'model': model,
                'session': model.Session,
                'user': model.User.get('testsysadmin').name}
@@ -23,7 +22,7 @@ def _create_test_view(view_type):
     package = model.Package.get('annakarenina')
     resource_id = package.resources[1].id
     resource_view = {'resource_id': resource_id,
-                     'view_type': view_type,
+                     'view_type': 'pdf_view',
                      'title': u'Test View',
                      'description': u'A *nice* test view'}
     resource_view = plugins.toolkit.get_action('resource_view_create')(
@@ -31,64 +30,68 @@ def _create_test_view(view_type):
     return resource_view, package, resource_id
 
 
+@pytest.fixture
+def pdf_view_plugin():
+    p = plugin.PdfView()
+    p.proxy_is_enabled = False
+    create_test_data.CreateTestData.create()
+    return p
+
+
+@pytest.mark.ckan_config(u'ckan.plugins', u'pdf_view')
+@pytest.mark.ckan_config(u'ckan.legacy_templates', u'false')
+@pytest.mark.usefixtures(u'with_plugins', u'with_request_context',
+                         u'app', u'clean_db', 'pdf_view_plugin')
 class TestPdfView(tests.WsgiAppCase):
     view_type = 'pdf_view'
 
-    @classmethod
-    def setup_class(cls):
-        cls.config_templates = config['ckan.legacy_templates']
-        config['ckan.legacy_templates'] = 'false'
-        wsgiapp = middleware.make_app(config['global_conf'], **config)
-
-        plugins.load('pdf_view')
-        cls.app = paste.fixture.TestApp(wsgiapp)
-        cls.p = plugin.PdfView()
-        cls.p.proxy_is_enabled = False
-
-        create_test_data.CreateTestData.create()
-
-        cls.resource_view, cls.package, cls.resource_id = \
-            _create_test_view(cls.view_type)
-
-    @classmethod
-    def teardown_class(cls):
-        config['ckan.legacy_templates'] = cls.config_templates
-        plugins.unload('pdf_view')
-        model.repo.rebuild_db()
-
-    def test_can_view(self):
+    def test_can_view(self, app, pdf_view_plugin):
         url_same_domain = urlparse.urljoin(
             config.get('ckan.site_url', '//localhost:5000'),
             '/resource.txt')
         url_different_domain = 'http://some.com/resource.pdf'
 
         data_dict = {'resource': {'format': 'pdf', 'url': url_same_domain}}
-        assert self.p.can_view(data_dict)
+        assert pdf_view_plugin.can_view(data_dict)
 
         data_dict = {'resource': {'format': 'x-pdf', 'url': url_same_domain}}
-        assert self.p.can_view(data_dict)
+        assert pdf_view_plugin.can_view(data_dict)
 
         data_dict = {'resource': {'format': 'pdf',
                                   'url': url_different_domain}}
-        assert not self.p.can_view(data_dict)
+        assert not pdf_view_plugin.can_view(data_dict)
 
-    def test_js_included(self):
-        url = h.url_for(controller='package', action='resource_view',
-                        id=self.package.name, resource_id=self.resource_id,
-                        view_id=self.resource_view['id'])
-        result = self.app.get(url)
-        assert (('pdf_view.js' in result.body) or
-                ('pdf_view.min.js' in result.body))
+    def test_js_included(self, app, pdf_view_plugin):
+        resource_view, package, resource_id = _create_test_view()
+        url = h.url_for('resource.view',
+                        id=package.name, resource_id=resource_id,
+                        view_id=resource_view['id'])
+        result = app.get(url)
+        print(result.body)
+        assert (('_pdfview.js' in result.body) or
+                ('_pdfview.min.js' in result.body))
 
-    def test_title_description_iframe_shown(self):
-        url = h.url_for(controller='package', action='resource_read',
-                        id=self.package.name, resource_id=self.resource_id)
-        result = self.app.get(url)
-        assert self.resource_view['title'] in result
+    def test_css_included(self, app, pdf_view_plugin):
+        resource_view, package, resource_id = _create_test_view()
+        url = h.url_for('resource.view',
+                        id=package.name, resource_id=resource_id,
+                        view_id=resource_view['id'])
+        result = app.get(url)
+        print(result.body)
+        assert (('_pdfview.css' in result.body) or
+                ('_pdfview.min.css' in result.body))
+
+    def test_title_description_iframe_shown(self, app, pdf_view_plugin):
+        resource_view, package, resource_id = _create_test_view()
+        url = h.url_for('resource.read',
+                        id=package.name, resource_id=resource_id)
+        result = app.get(url)
+        assert resource_view['title'] in result
         assert 'data-module="data-viewer"' in result.body
 
-    def test_description_supports_markdown(self):
-        url = h.url_for(controller='package', action='resource_read',
-                        id=self.package.name, resource_id=self.resource_id)
-        result = self.app.get(url)
+    def test_description_supports_markdown(self, app, pdf_view_plugin):
+        resource_view, package, resource_id = _create_test_view()
+        url = h.url_for('resource.read',
+                        id=package.name, resource_id=resource_id)
+        result = app.get(url)
         assert 'A <em>nice</em> test view' in result
